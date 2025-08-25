3a93b99568c5d2e17fcc82a69420bc42a13a36c1
Vladyslav Boychuk <vboychuk1122@gmail.com>
2025-08-25 23:25:29 +0200

    Second try to diasable gyroscope

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -23,6 +23,7 @@ const ProjectCard = memo(
 					tiltMaxAngleY={15}
 					transitionSpeed={300}
 					scale={1.04}
+					gyroscope={false}
 					className="bg-primary p-5 rounded-2xl w-full h-full 
 						shadow-lg shadow-black/30 
 						hover:shadow-2xl hover:shadow-black/50 
