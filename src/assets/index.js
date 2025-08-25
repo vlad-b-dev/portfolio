d9d6f0786b701bd23a03de216decee2d0f9d616c
Vladyslav Boychuk <vboychuk1122@gmail.com>
2025-08-25 08:55:32 +0200

    - Rename to correct name again

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,7 +20,7 @@ import bmwaysLogo from "./experience/bmwaysLogo.webp";
 
 import coinPulseLogo from "./experience/coinPulseLogo.webp";
 import kilaAppLogo from "./experience/kilaAppLogo.webp";
-import vibeCastLogo from "./experience/vibeCast.webp";
+import vibeCastLogo from "./experience/vibeCastLogo.webp";
 
 export {
 	logo,
